e34109b331460ea2940735d582912d3813702ca7
robb_joe <robb_joe@hotmail.co.uk>
2020-02-05 10:28:41 +0000

    Updates

---
+++ precache-manifest.055482142efcbe411b7d195cdec6c8c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058adb3d991085025f9f9fbb68198878",
+    "url": "/React-Scoreboard/index.html"
+  },
+  {
+    "revision": "1f9f612cc766c09dfb74",
+    "url": "/React-Scoreboard/static/css/main.e46c872e.chunk.css"
+  },
+  {
+    "revision": "0156ae93402580dd7fbe",
+    "url": "/React-Scoreboard/static/js/2.900dada2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Scoreboard/static/js/2.900dada2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f9f612cc766c09dfb74",
+    "url": "/React-Scoreboard/static/js/main.af83fc75.chunk.js"
+  },
+  {
+    "revision": "1ce8bd38dfaf0db29ead",
+    "url": "/React-Scoreboard/static/js/runtime-main.b1aff3ba.js"
+  }
+]);
